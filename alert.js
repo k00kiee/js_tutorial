c9907f22b7d00b9769f3531ecadf94712d5db633
Koki Ouchi <s1180087@gmail.com>
2021-06-29 17:41:56 +0900

    オブジェクトの確認

--- alert.js
+++ alert.js
@@ -1,19 +1,11 @@
-let ask = (question, yes, no) => confirm(question) ? yes():no();
+let user = new Object();
+user = {
+    name:"john",
+    surname:"Smith",
+}
 
-ask(
-    "Do you agree?",
-    () => alert("You agreed."),
-    () => alert("You canceled the execution.")
-);
+user[name] = "Pete";
+alert(user[name]);
 
-
-// function ask(question, yes, no) {
-//     if (confirm(question)) yes()
-//     else no();
-//   }
-  
-//   ask(
-//     "Do you agree?",
-//     function() { alert("You agreed."); },
-//     function() { alert("You canceled the execution."); }
-//   );
+delete user[name];
+alert(user[name]);
